aa9ef0eae08d3845fb2dab09d99d48246daef38e
pas <pas@zomg.hu>
2018-03-09 11:24:57 +0100

    fixup

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -4,4 +4,4 @@ set -e
 
 cd $(dirname $(readlink -f $0))
 
-docker run -v /run/docker.sock:/var/run/docker.sock -v $PWD:/build anapsix/alpine-java:8_jdk_unlimited bash -c "test -S /run/docker.sock && test -d /build && apk update && apk add docker wget && cd /build && ./sbt docker:publishLocal"
+docker run -v /run/docker.sock:/var/run/docker.sock -v $PWD:/build anapsix/alpine-java:8_jdk_unlimited bash -c "test -S /var/run/docker.sock && test -d /build && apk update && apk add docker wget && cd /build && ./sbt docker:publishLocal"
